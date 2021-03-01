d09b540abc9b6fe9a7f927b5333bc15546cacae9
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-03-01 11:48:16 +0900

    prisma setting

---
+++ server.js
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import {ApolloServer, gql} from "apollo-server";
+
+const client = new PrismaClient();
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Movie{
+    id:Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updateAt: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation{
+    createMovie(title: String!, year:Int!, genre:String):Movie
+    deleteMovie(title: String!):Boolean
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: () => ({title: "Hello", year: 2021}),
+  },
+  Mutation: {
+    createMovie: (_, {title, year, genre})=>{
+      client.movie.create({
+        data:{
+          title,
+          year,
+          genre,
+      }})
+    },
+    deleteMovie: (_, {title}) =>{
+      console.log(title);
+      return true;
+    },
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(() => {
+  console.log(`🚀 Server ready at http://localhost:4000/`);
+});

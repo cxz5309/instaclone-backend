dedf8790458ac300cf9613a2f9c3d8c72844a216
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-03-02 17:45:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const typeDefs = gql`
     createdAt: String!
     updateAt: String!
   }
+  type Mutation{
+    createMovie(title: String!, year: Int!, genre: String):Movie
+    deleteMovie(id: String!):Boolean
+  }
   type Query {
     movies: [Movie]
     movie: Movie
-  }
-  type Mutation{
-    createMovie(title: String!, year:Int!, genre:String):Movie
-    deleteMovie(title: String!):Boolean
   }
 `;
 
@@ -27,7 +27,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     movies: () => client.movie.findMany(),
-    movie: () => ({title: "Hello", year: 2021}),
+    movie: () =>({title:"hello", year:2021}),
   },
   Mutation: {
     createMovie: (_, {title, year, genre})=>{
@@ -53,3 +53,59 @@ const server = new ApolloServer({
 server.listen().then(() => {
   console.log(`🚀 Server ready at http://localhost:4000/`);
 });
+
+// import { PrismaClient } from "@prisma/client";
+// import {ApolloServer, gql} from "apollo-server";
+
+// const client = new PrismaClient();
+
+// // The GraphQL schema
+// const typeDefs = gql`
+//   type Movie{
+//     id:Int!
+//     title: String!
+//     year: Int!
+//     genre: String
+//     createdAt: String!
+//     updateAt: String!
+//   }
+//   type Query {
+//     movies: [Movie]
+//     Movie: Movie
+//   }
+//   type Mutation{
+//     createMovie(title: String!, year: Int!, genre: String):Movie
+//     deleteMovie(id: String!):Boolean
+//   }
+// `;
+
+// // A map of functions which return data for the schema.
+// const resolvers = {
+//   Query: {
+//     movies: () => client.movie.findMany(),
+//     movie: () =>({title:"hello", year:2021})
+//   },
+//   Mutation: {
+//     createMovie: (_, {title, year, genre})=>{
+//       client.movie.create({
+//         data:{
+//           title,
+//           year,
+//           genre,
+//       }})
+//     },
+//     deleteMovie: (_, {title}) =>{
+//       console.log(title);
+//       return true;
+//     },
+//   }
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// server.listen().then(() => {
+//   console.log(`🚀 Server ready at http://localhost:4000/`);
+// });
